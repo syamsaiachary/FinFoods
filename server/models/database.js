e1341ef0125c8b65d74182744bb4deccb4cc4169
syam sai <syamsaiachary@gmail.com>
2024-09-17 18:17:43 +0530

    FinFoods
    version --0.0

---
+++ server/models/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('Connected')
+});
+
+//Models
+
+require('./Category');
+require('./Recipe');
